5f01b622a6fb0afaf1dd076a2a975c2da376b590
Alex <hi@alexmenger.me>
2023-01-03 13:55:36 +0100

    Enhance user model validation

--- server/models/User.js
+++ server/models/User.js
@@ -1,26 +1,31 @@
 import mongoose from 'mongoose';
+import isEmail from 'validator/lib/isEmail.js';
 
 const userSchema = new mongoose.Schema(
     {
         firstName: {
             type: String,
-            required: true,
+            required: [true, 'First name is required'],
             trim: true
         },
         lastName: {
             type: String,
-            required: true,
+            required: [true, 'Last name is required'],
             trim: true
         },
         email: {
             type: String,
-            required: true,
-            unique: true,
-            trim: true
+            required: [true, 'Email address is required'],
+            unique: [true, 'Email address already taken'],
+            trim: true,
+            validate: {
+                validator: val => isEmail(val),
+                message: 'Email address is invalid'
+            }
         },
         password: {
             type: String,
-            required: true,
+            required: [true, 'Password is required'],
             trim: true
         }
     },
