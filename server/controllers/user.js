1e9f2ebc4bd814a68e2f654f00f94045551a648e
Alex <hi@alexmenger.me>
2023-01-03 13:55:23 +0100

    Add user controller

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,5 +1,16 @@
+import User from '../models/User.js';
+import extractSchemaErrors from '../utils/extractSchemaErrors.js';
+
 const userController = {
-    signUp: (req, res) => res.json({ signup: true })
+    signUp: async (req, res, next) => {
+        try {
+            const newUser = new User(req.body);
+            await newUser.save();
+            res.status(201).json({ signup: true });
+        } catch (err) {
+            next(extractSchemaErrors(err));
+        }
+    }
 };
 
 export default userController;
