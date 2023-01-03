63076be1051af63cf0f01a8d55e135e3ba38aa2b
Alex <hi@alexmenger.me>
2023-01-03 17:50:32 +0100

    Add login route

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
 import userController from '../controllers/user.js';
+import { authLocal, authJWT } from '../utils/auth.js';
 
 const usersRouter = Router();
 
 usersRouter.route('/signup').post(userController.signUp);
+usersRouter.route('/login').post(authLocal, userController.logIn);
 
 export default usersRouter;
