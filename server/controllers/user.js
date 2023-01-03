05d60f61ec5f19b410b385bb8fc29998f6f48f7a
Alex <hi@alexmenger.me>
2023-01-03 17:48:49 +0100

    Add login controller

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,6 +1,17 @@
 import User from '../models/User.js';
 
 const userController = {
+    logIn: async (req, res) => {
+        const token = await req.user.generateToken();
+        const loginDuration = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000);
+        res.status(200)
+            .cookie('auth', token, {
+                httpOnly: true,
+                expires: loginDuration,
+                secure: process.env.NODE_ENV !== 'development'
+            })
+            .json(req.user);
+    },
     signUp: async (req, res, next) => {
         try {
             const newUser = new User(req.body);
