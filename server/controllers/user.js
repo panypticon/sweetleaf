726b03267a3eca47c31b2f2d1efd9530aa8ea4ba
Alex <hi@alexmenger.me>
2023-01-04 16:19:31 +0100

    Enhance login Controller for third-party redirects

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,17 +1,26 @@
 import User from '../models/User.js';
 
 const userController = {
-    logIn: async (req, res) => {
-        const token = await req.user.generateToken();
-        const loginDuration = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000);
-        res.status(200)
-            .cookie('auth', token, {
-                httpOnly: true,
-                expires: loginDuration,
-                secure: process.env.NODE_ENV !== 'development'
-            })
-            .json(req.user);
-    },
+    logIn:
+        (isThirdParty = false) =>
+        async (req, res) => {
+            const token = await req.user.generateToken();
+            const loginDuration = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000);
+            if (!isThirdParty)
+                res.status(200)
+                    .cookie('auth', token, {
+                        httpOnly: true,
+                        expires: loginDuration,
+                        secure: process.env.NODE_ENV !== 'development'
+                    })
+                    .json(req.user);
+            else
+                res.cookie('auth', token, {
+                    httpOnly: true,
+                    expires: loginDuration,
+                    secure: process.env.NODE_ENV !== 'development'
+                }).redirect('/');
+        },
     signUp: async (req, res, next) => {
         try {
             const newUser = new User(req.body);
