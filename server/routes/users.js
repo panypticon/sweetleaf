8d9c88387c629aaa7b6a4e4b7463354e17559bc2
Alex <hi@alexmenger.me>
2023-01-04 16:19:46 +0100

    Add routes for Google auth

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 
 import userController from '../controllers/user.js';
-import { authLocal, authJWT } from '../utils/auth.js';
+import { authLocal, authJWT, authGoogle } from '../utils/auth.js';
 
 const usersRouter = Router();
 
 usersRouter.route('/signup').post(userController.signUp);
-usersRouter.route('/login').post(authLocal, userController.logIn);
+usersRouter.route('/login').post(authLocal, userController.logIn());
+usersRouter.route('/login/google').get(authGoogle);
+usersRouter.route('/login/google/redirect').get(authGoogle, userController.logIn(true));
 
 export default usersRouter;
