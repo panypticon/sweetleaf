bb7cedb6f894129bf2b1d3ac2c21d938b006431e
Alex <hi@alexmenger.me>
2023-01-04 16:20:37 +0100

    Add Google strategy

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,15 +1,19 @@
 import passport from 'passport';
 import LocalStrategy from 'passport-local';
 import { Strategy as JWTStrategy, ExtractJwt } from 'passport-jwt';
+import GoogleStrategy from 'passport-google-oauth20';
 import createError from 'http-errors';
 import 'dotenv/config';
 
 import User from '../models/User.js';
+import GoogleUser from '../models/GoogleUser.js';
+
+const { JWT_SECRET, GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, NODE_ENV } = process.env;
 
 passport.use(
     new LocalStrategy({ usernameField: 'email', passwordField: 'password' }, async (email, password, done) => {
         try {
-            const user = await User.findOne({ email });
+            const user = await User.findOne({ email, password: { $exists: true } });
             if (!user) throw createError.Unauthorized();
             const passwordsMatch = await user.authenticate(password);
             if (!passwordsMatch) throw createError.Unauthorized();
@@ -23,7 +27,7 @@ passport.use(
 passport.use(
     new JWTStrategy(
         {
-            secretOrKey: process.env.JWT_SECRET,
+            secretOrKey: JWT_SECRET,
             jwtFromRequest: ExtractJwt.fromExtractors([
                 req => {
                     if (!req.cookies.auth) throw createError.Unauthorized();
@@ -43,5 +47,38 @@ passport.use(
     )
 );
 
+passport.use(
+    new GoogleStrategy(
+        {
+            clientID: GOOGLE_CLIENT_ID,
+            clientSecret: GOOGLE_CLIENT_SECRET,
+            callbackURL: `${
+                NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://sweetleaf.vercel.app'
+            }/api/v1/users/login/google/redirect`,
+            scope: ['profile', 'email'],
+            state: false
+        },
+        async (_accessToken, _refreshToken, { id, name, emails }, done) => {
+            try {
+                const user = await GoogleUser.findOne({ googleID: id });
+                if (user) return done(null, user);
+                else {
+                    const newUser = new GoogleUser({
+                        firstName: name.givenName || 'J.',
+                        lastName: name.familyName || 'Doe',
+                        email: emails[0].value,
+                        googleID: id
+                    });
+                    await newUser.save();
+                    return done(null, newUser);
+                }
+            } catch (err) {
+                return done(err);
+            }
+        }
+    )
+);
+
 export const authLocal = passport.authenticate('local', { session: false });
 export const authJWT = passport.authenticate('jwt', { session: false });
+export const authGoogle = passport.authenticate('google', { session: false });
