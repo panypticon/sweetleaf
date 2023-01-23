f7e68e6019e31e8830be2d3142c780c2486d6de1
Alex <hi@alexmenger.me>
2023-01-23 18:56:50 +0100

    Reorder routes

--- server/routes/users.js
+++ server/routes/users.js
@@ -6,14 +6,14 @@ import { authLocal, authGoogle, isAdmin, isUserOrAdmin } from '../utils/auth.js'
 const usersRouter = Router();
 
 usersRouter.route('/').get(isAdmin, userController.getAll);
+usersRouter.route('/signup').post(userController.signUp);
+usersRouter.route('/login').post(authLocal, userController.logIn());
+usersRouter.route('/login/google').get(authGoogle);
+usersRouter.route('/login/google/redirect').get(authGoogle, userController.logIn(true));
 usersRouter
     .route('/:id')
     .get(isUserOrAdmin, userController.getOne)
     .put(isUserOrAdmin, userController.update)
     .delete(isUserOrAdmin, userController.delete);
-usersRouter.route('/signup').post(userController.signUp);
-usersRouter.route('/login').post(authLocal, userController.logIn());
-usersRouter.route('/login/google').get(authGoogle);
-usersRouter.route('/login/google/redirect').get(authGoogle, userController.logIn(true));
 
 export default usersRouter;
