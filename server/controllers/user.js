5f756abf6690150f5dd02af9b62d92d3c0b40751
Alex <hi@alexmenger.me>
2023-01-24 17:18:41 +0100

    Remove empty query object

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -16,7 +16,7 @@ const userController = {
     },
     getAll: async (_, res, next) => {
         try {
-            const users = await User.find({});
+            const users = await User.find();
             res.status(200).json(users);
         } catch (err) {
             next(err);
