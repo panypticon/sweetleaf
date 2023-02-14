58f678327c6b80fce29ed06c5fadade71bb30c21
Alex <hi@alexmenger.me>
2023-02-14 12:56:56 +0100

    Implement Product page

--- client/src/routes/product/product.styled.ts
+++ client/src/routes/product/product.styled.ts
@@ -1,6 +1,31 @@
 import styled from 'styled-components';
 
+import { defaults, colors } from '../../root.styled';
+
 export const StyledProduct = styled.article`
+    h1 {
+        color: ${colors.contrast.dark};
+        margin-bottom: calc(${defaults.layoutPadding} * 2);
+    }
+
     .Product {
+        &__section {
+            padding: 0 ${defaults.layoutPadding};
+            margin: ${defaults.layoutPadding} 0;
+        }
+
+        &__main {
+            display: flex;
+            gap: ${defaults.layoutPadding};
+
+            img {
+                mix-blend-mode: multiply;
+                width: calc(38.19660113% - (${defaults.layoutPadding} / 2));
+            }
+        }
+
+        &__data {
+            flex: 1;
+        }
     }
 `;
--- client/src/routes/product/product.tsx
+++ client/src/routes/product/product.tsx
@@ -3,25 +3,37 @@ import { Breadcrumb } from 'antd';
 import capitalize from 'lodash/capitalize';
 import { Link } from 'react-router-dom';
 
+import PackageSelector from '../../components/packageselector/packageselector';
+
 import type { Product } from '../../types';
 
 import { StyledProduct } from './product.styled';
 
 const ProductPage = (): JSX.Element => {
-    const data = useLoaderData() as Product;
-    console.log(data);
+    const { type, category, name, image, description, id, inventory } = useLoaderData() as Product;
+
+    console.log(inventory);
 
     return (
         <StyledProduct className="Product">
             <Breadcrumb>
                 <Breadcrumb.Item>
-                    <Link to={`/${data.type}`}>{capitalize(data.type)}</Link>
+                    <Link to={`/${type}`}>{capitalize(type)}</Link>
                 </Breadcrumb.Item>
                 <Breadcrumb.Item>
-                    <Link to={`/${data.type}/${data.category}`}>{capitalize(data.category)}</Link>
+                    <Link to={`/${type}/${category}`}>{capitalize(category)}</Link>
                 </Breadcrumb.Item>
             </Breadcrumb>
-            Product {data.type} {data.id}
+            <section className="Product__section">
+                <h1>{name}</h1>
+                <div className="Product__main">
+                    <img src={image} alt="name" />
+                    <div className="Product__data">
+                        <div>{description}</div>
+                        <PackageSelector id={id} inventory={inventory} />
+                    </div>
+                </div>
+            </section>
         </StyledProduct>
     );
 };
