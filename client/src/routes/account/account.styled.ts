a12b2788897ba55ce4163c8b3fc357ee3f7ac70f
Alex <hi@alexmenger.me>
2023-02-23 14:53:09 +0100

    Implement account page

--- client/src/routes/account/account.styled.ts
+++ client/src/routes/account/account.styled.ts
@@ -1,3 +1,50 @@
 import styled from 'styled-components';
 
-export const StyledAccount = styled.article``;
+import { defaults, colors } from '../../root.styled';
+
+export const StyledAccount = styled.article`
+    padding: 0 ${defaults.layoutPadding};
+    margin: ${defaults.layoutPadding} 0;
+
+    h1 {
+        margin-bottom: calc(${defaults.layoutPadding} * 2);
+    }
+
+    h2 {
+        color: ${colors.contrast.dark};
+        margin-bottom: ${defaults.layoutPadding};
+    }
+
+    .Account {
+        &__form-column {
+            display: flex;
+            gap: ${defaults.layoutPadding};
+
+            > * {
+                flex: 1;
+            }
+        }
+
+        &__status {
+            display: flex;
+            gap: ${defaults.layoutPadding};
+            align-items: center;
+
+            .anticon {
+                margin-right: 0.8rem;
+            }
+
+            .success {
+                .anticon {
+                    color: ${colors.steamed.standard};
+                }
+            }
+
+            .error {
+                .anticon {
+                    color: ${colors.herbal.standard};
+                }
+            }
+        }
+    }
+`;
--- client/src/routes/account/account.tsx
+++ client/src/routes/account/account.tsx
@@ -1,9 +1,15 @@
 import { Form, Input, Select } from 'antd';
+import { CheckOutlined, ExclamationOutlined } from '@ant-design/icons';
 
 import { selectGlobalData } from '../../store/slices/globalData';
-import { useAppSelector } from '../../store/hooks';
+import { useAppSelector, useAppDispatch } from '../../store/hooks';
 import Button from '../../components/button/button';
 import useAuthProtection from '../../hooks/useAuthProtection';
+import { postJSONData } from '../../api/fetch';
+import { setUser } from '../../store/slices/globalData';
+import useTimedRequestState from '../../hooks/useTimedRequestState';
+
+import type { User } from '../../types';
 
 import { StyledAccount } from './account.styled';
 
@@ -14,88 +20,122 @@ const countryOptions = [
 ];
 
 const Account = (): JSX.Element => {
-    const { user } = useAppSelector(selectGlobalData);
+    const [nameAddressState, setNameAddressState] = useTimedRequestState(2000);
 
+    const { user } = useAppSelector(selectGlobalData);
     useAuthProtection(user);
 
     const [form] = Form.useForm();
+
+    const dispatch = useAppDispatch();
+
+    const handleNameAddressSubmit = async (vals: User) => {
+        try {
+            const newUser = await postJSONData(`/api/v1/users/${user?.id}`, { address: vals }, 'PUT');
+            newUser && dispatch(setUser(newUser));
+            setNameAddressState({ success: true, error: false });
+        } catch (err) {
+            setNameAddressState({ success: false, error: true });
+        }
+    };
 
     return (
         <StyledAccount className="Account">
             {user && (
                 <>
                     <h1>Account Data</h1>
-                    <h2>Name and Address</h2>
-                    <Form
-                        form={form}
-                        name="name_address"
-                        onFinish={vals => console.log(vals)}
-                        layout="vertical"
-                        requiredMark={false}
-                        validateTrigger="onBlur"
-                    >
-                        <Form.Item
-                            name="firstName"
-                            label="First Name"
-                            rules={[
-                                { required: true, message: 'First name is required' },
-                                { max: 128, message: 'First name is too long' }
-                            ]}
-                            initialValue={user.address.firstName}
+                    <section className="Account__section">
+                        <h2>Name and Address</h2>
+                        <Form
+                            form={form}
+                            name="name_address"
+                            onFinish={handleNameAddressSubmit}
+                            layout="vertical"
+                            requiredMark={false}
+                            validateTrigger="onBlur"
                         >
-                            <Input />
-                        </Form.Item>
-                        <Form.Item
-                            name="lastName"
-                            label="Last Name"
-                            rules={[
-                                { required: true, message: 'Last name is required' },
-                                { max: 128, message: 'Last name is too long' }
-                            ]}
-                            initialValue={user.address.lastName}
-                        >
-                            <Input />
-                        </Form.Item>
-                        <Form.Item
-                            name="street"
-                            label="Street address"
-                            rules={[
-                                { required: true, message: 'Street address is required' },
-                                { max: 266, message: 'Street address is too long' }
-                            ]}
-                            initialValue={user.address.street}
-                        >
-                            <Input />
-                        </Form.Item>
-                        <Form.Item
-                            name="city"
-                            label="City"
-                            rules={[
-                                { required: true, message: 'City is required' },
-                                { max: 128, message: 'City is too long' }
-                            ]}
-                            initialValue={user.address.city}
-                        >
-                            <Input />
-                        </Form.Item>
-                        <Form.Item
-                            name="zip"
-                            label="ZIP"
-                            rules={[
-                                { required: true, message: 'ZIP is required' },
-                                { max: 32, message: 'ZIP is too long' }
-                            ]}
-                            initialValue={user.address.zip}
-                        >
-                            <Input />
-                        </Form.Item>
-                        <Form.Item name="country" label="Country" initialValue={user.address.country}>
-                            <Select options={countryOptions} />
-                        </Form.Item>
-                        <Button type="primary" htmlType="submit">
-                            Update
-                        </Button>
-                    </Form>
+                            <span className="Account__form-column">
+                                <Form.Item
+                                    name="firstName"
+                                    label="First Name"
+                                    rules={[
+                                        { required: true, message: 'First name is required' },
+                                        { max: 128, message: 'First name is too long' }
+                                    ]}
+                                    initialValue={user.address.firstName}
+                                >
+                                    <Input />
+                                </Form.Item>
+                                <Form.Item
+                                    name="lastName"
+                                    label="Last Name"
+                                    rules={[
+                                        { required: true, message: 'Last name is required' },
+                                        { max: 128, message: 'Last name is too long' }
+                                    ]}
+                                    initialValue={user.address.lastName}
+                                >
+                                    <Input />
+                                </Form.Item>
+                            </span>
+                            <Form.Item
+                                name="street"
+                                label="Street address"
+                                rules={[
+                                    { required: true, message: 'Street address is required' },
+                                    { max: 266, message: 'Street address is too long' }
+                                ]}
+                                initialValue={user.address.street}
+                            >
+                                <Input />
+                            </Form.Item>
+                            <span className="Account__form-column">
+                                <Form.Item
+                                    name="city"
+                                    label="City"
+                                    rules={[
+                                        { required: true, message: 'City is required' },
+                                        { max: 128, message: 'City is too long' }
+                                    ]}
+                                    initialValue={user.address.city}
+                                >
+                                    <Input />
+                                </Form.Item>
+                                <Form.Item
+                                    name="zip"
+                                    label="ZIP"
+                                    rules={[
+                                        { required: true, message: 'ZIP is required' },
+                                        { max: 32, message: 'ZIP is too long' }
+                                    ]}
+                                    initialValue={user.address.zip}
+                                >
+                                    <Input />
+                                </Form.Item>
+                            </span>
+                            <Form.Item name="country" label="Country" initialValue={user.address.country}>
+                                <Select options={countryOptions} />
+                            </Form.Item>
+                            <div className="Account__status">
+                                <Button type="primary" htmlType="submit">
+                                    Update
+                                </Button>
+                                {nameAddressState.success ? (
+                                    <span className="success">
+                                        <CheckOutlined />
+                                        Changes saved
+                                    </span>
+                                ) : nameAddressState.error ? (
+                                    <span className="error">
+                                        <ExclamationOutlined />
+                                        Error saving changes, try again later
+                                    </span>
+                                ) : (
+                                    <></>
+                                )}
+                            </div>
+                        </Form>
+                    </section>
                     <h2>Email</h2>
                     <h2>Password</h2>
                 </>
