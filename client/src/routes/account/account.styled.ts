faadee082ee9f742980be6895e5e6c8772106090
Alex <hi@alexmenger.me>
2023-02-23 17:28:33 +0100

    Style account page

--- client/src/routes/account/account.styled.ts
+++ client/src/routes/account/account.styled.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-import { defaults, colors } from '../../root.styled';
+import { defaults, colors, breakpoints } from '../../root.styled';
 
 export const StyledAccount = styled.article`
     padding: 0 ${defaults.layoutPadding};
@@ -13,6 +13,15 @@ export const StyledAccount = styled.article`
     h2 {
         color: ${colors.contrast.dark};
         margin-bottom: ${defaults.layoutPadding};
+    }
+
+    form {
+        max-width: calc(61.80339887% - (${defaults.layoutPadding} / 2));
+
+        @media (max-width: ${breakpoints.lg}) {
+            width: 100%;
+            max-width: unset;
+        }
     }
 
     .Account {
@@ -43,6 +52,18 @@ export const StyledAccount = styled.article`
             .error {
                 .anticon {
                     color: ${colors.herbal.standard};
+                }
+            }
+        }
+
+        &__section {
+            &:not(:last-of-type) {
+                margin-bottom: calc(${defaults.layoutPadding} * 2);
+            }
+
+            &--password {
+                form div.ant-form-item:not(:first-child) {
+                    margin-bottom: 0;
                 }
             }
         }
