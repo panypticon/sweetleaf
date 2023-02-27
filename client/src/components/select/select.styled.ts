bdb9e6553c83fe44997c8b0095355a531bb19e8f
Alex <hi@alexmenger.me>
2023-02-27 10:26:54 +0100

    Style select

--- client/src/components/select/select.styled.ts
+++ client/src/components/select/select.styled.ts
@@ -1,5 +1,6 @@
 import { Select } from 'antd';
 import styled from 'styled-components';
+import { rgba } from 'polished';
 
 import { colors } from '../../root.styled';
 
@@ -18,6 +19,42 @@ const StyledSelect = styled(Select<SelectProps>)`
                 .ant-select-selector {
                     border-color: ${colors.steamed.standard};
                 }
+            }
+        }
+
+        &-open {
+            .ant-select-selector {
+                border-bottom-left-radius: 0;
+                border-bottom-right-radius: 0;
+            }
+        }
+
+        &-focused:not(.ant-select-disabled):not(.ant-select-customize-input):not(.ant-pagination-size-changer) {
+            .ant-select-selector {
+                box-shadow: none;
+            }
+        }
+    }
+
+    .ant-select {
+        &-dropdown {
+            padding: 0;
+            box-shadow: 0 0 1.2rem ${rgba(colors.contrast.dark, 0.1)};
+            border: 0.1rem solid ${colors.contrast['shade-6']};
+            border-top-left-radius: 0;
+            border-top-right-radius: 0;
+        }
+
+        &-item {
+            border-radius: unset;
+
+            &-option-active:not(.ant-select-item-option-disabled) {
+                background-color: ${rgba(colors.contrast['shade-6'], 0.33333)};
+            }
+
+            &-option-selected:not(.ant-select-item-option-disabled) {
+                background-color: ${rgba(colors.fermented['lighter-4'], 0.33333)};
+                font-weight: 400;
             }
         }
     }
--- client/src/components/select/select.tsx
+++ client/src/components/select/select.tsx
@@ -2,6 +2,14 @@ import StyledSelect from './select.styled';
 
 import type { SelectProps } from 'antd';
 
-const Select = (props: SelectProps): JSX.Element => <StyledSelect {...props} />;
+const Select = (props: SelectProps): JSX.Element => (
+    <StyledSelect
+        getPopupContainer={trigger => trigger.closest('.ant-select')}
+        dropdownAlign={{
+            offset: [0, 0]
+        }}
+        {...props}
+    />
+);
 
 export default Select;
