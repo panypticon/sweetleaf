7760e35cd8c8a95169a719a60f9cc06ce660c4ee
Alex <hi@alexmenger.me>
2023-02-28 12:47:16 +0100

    Implement checkout

--- client/src/routes/checkout/checkout.styled.ts
+++ client/src/routes/checkout/checkout.styled.ts
@@ -16,9 +16,28 @@ export const StyledCheckout = styled.article`
         &__section {
             padding: 0 ${defaults.layoutPadding};
             margin: ${defaults.layoutPadding} 0;
+            max-width: 61.80339887%;
+
+            @media (max-width: ${breakpoints.lg}) {
+                width: 100%;
+                max-width: unset;
+            }
 
             &:not(:first-of-type) {
                 margin: calc(${defaults.layoutPadding} * 3) 0;
+            }
+        }
+
+        &__form-row {
+            display: flex;
+            gap: ${defaults.layoutPadding};
+
+            > * {
+                flex: 1;
+            }
+
+            @media (max-width: ${breakpoints.xs}) {
+                display: block;
             }
         }
 
@@ -34,6 +53,35 @@ export const StyledCheckout = styled.article`
                 .Button {
                     width: 100%;
                     margin: calc(${defaults.layoutPadding} / 2) 0;
+                }
+            }
+        }
+
+        &--cart {
+            .ant-form > div:not(:last-child) {
+                margin-bottom: calc(${defaults.layoutPadding} * 2);
+            }
+
+            &__total {
+                display: flex;
+                justify-content: flex-end;
+                gap: ${defaults.layoutPadding};
+                font-weight: 600;
+                padding-top: calc(${defaults.layoutPadding} / 2);
+                border-top: 0.2rem solid ${colors.contrast['shade-6']};
+                margin-top: calc(${defaults.layoutPadding} / 2);
+            }
+
+            &__confirm {
+                display: flex;
+                justify-content: flex-end;
+                align-items: center;
+
+                @media (max-width: ${breakpoints.sm}) {
+                    .Button {
+                        align-self: stretch;
+                        width: 100%;
+                    }
                 }
             }
         }
--- client/src/routes/checkout/checkout.tsx
+++ client/src/routes/checkout/checkout.tsx
@@ -1,5 +1,6 @@
 import { useContext, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { Form, Input } from 'antd';
 
 import { useAppSelector } from '../../store/hooks';
 import { selectGlobalData } from '../../store/slices/globalData';
@@ -10,6 +11,8 @@ import SignupModal from '../../components/signupmodal/signupmodal';
 import Button from '../../components/button/button';
 import useCartData from '../../hooks/useCartData';
 import CartItem from '../../components/cartitem/cartitem';
+import Select from '../../components/select/select';
+import countryOptions from '../../components/select/countryoptions';
 
 import StyledCheckout from './checkout.styled';
 
@@ -23,6 +26,8 @@ const Checkout = (): JSX.Element => {
 
     const { totalPrice, isEmpty, cartItems } = useCartData(cart);
 
+    const [form] = Form.useForm();
+
     useEffect(() => {
         isEmpty && navigate('/');
     }, [isEmpty, navigate]);
@@ -30,16 +35,103 @@ const Checkout = (): JSX.Element => {
     return (
         <StyledCheckout className="Checkout">
             <section className="Checkout__section">
-                <h1>Check Out Order</h1>
+                <h1>Check Out</h1>
                 {user ? (
-                    <div className="Checkout--Cart">
-                        <h3>Your Cart</h3>
-                        <ul>
-                            {cartItems.map(([id, item]) => (
-                                <CartItem key={id} data={{ ...item, id }} showImage={true} />
-                            ))}
-                        </ul>
-                        <p>{totalPrice}</p>
+                    <div className="Checkout--cart">
+                        <Form
+                            form={form}
+                            name="checkout_data"
+                            onFinish={address => {
+                                console.log({ address, cart });
+                            }}
+                            layout="vertical"
+                            requiredMark={false}
+                            validateTrigger="onBlur"
+                            className="Checkout__form"
+                            initialValues={user.address}
+                        >
+                            <div>
+                                <h3>Your Cart</h3>
+                                <ul>
+                                    {cartItems.map(([id, item]) => (
+                                        <CartItem key={id} data={{ ...item, id }} showImage={true} />
+                                    ))}
+                                </ul>
+                                <div className="Checkout--cart__total">
+                                    <span>Total</span>
+                                    <span>{`${totalPrice} €`}</span>
+                                </div>
+                            </div>
+                            <div>
+                                <h3>Shipping Address</h3>
+                                <span className="Checkout__form-row">
+                                    <Form.Item
+                                        name="firstName"
+                                        label="First Name"
+                                        rules={[
+                                            { required: true, message: 'First name is required' },
+                                            { max: 128, message: 'First name is too long' }
+                                        ]}
+                                    >
+                                        <Input />
+                                    </Form.Item>
+                                    <Form.Item
+                                        name="lastName"
+                                        label="Last Name"
+                                        rules={[
+                                            { required: true, message: 'Last name is required' },
+                                            { max: 128, message: 'Last name is too long' }
+                                        ]}
+                                    >
+                                        <Input />
+                                    </Form.Item>
+                                </span>
+                                <Form.Item
+                                    name="street"
+                                    label="Street address"
+                                    rules={[
+                                        { required: true, message: 'Street address is required' },
+                                        { max: 266, message: 'Street address is too long' }
+                                    ]}
+                                >
+                                    <Input />
+                                </Form.Item>
+                                <span className="Checkout__form-row">
+                                    <Form.Item
+                                        name="city"
+                                        label="City"
+                                        rules={[
+                                            { required: true, message: 'City is required' },
+                                            { max: 128, message: 'City is too long' }
+                                        ]}
+                                    >
+                                        <Input />
+                                    </Form.Item>
+                                    <Form.Item
+                                        name="zip"
+                                        label="ZIP"
+                                        rules={[
+                                            { required: true, message: 'ZIP is required' },
+                                            { max: 32, message: 'ZIP is too long' }
+                                        ]}
+                                    >
+                                        <Input />
+                                    </Form.Item>
+                                </span>
+                                <Form.Item name="country" label="Country">
+                                    <Select options={countryOptions} />
+                                </Form.Item>
+                            </div>
+                            <div>
+                                <h3>Payment</h3>
+                                <p>This is a demo. We don't want your money, and there are no goods to ship. ☺️</p>
+                            </div>
+                            <div className="Checkout--cart__confirm">
+                                <Button type="primary" htmlType="submit">
+                                    Order now
+                                </Button>
+                            </div>
+                        </Form>
                     </div>
                 ) : (
                     <div className="Checkout--login">
