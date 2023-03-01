73981b36cf8a4b888e219246481184dc08670052
Alex <hi@alexmenger.me>
2023-03-01 12:57:59 +0100

    Implement orders page

--- client/src/routes/account/orders.tsx
+++ client/src/routes/account/orders.tsx
@@ -2,15 +2,38 @@ import { useLoaderData } from 'react-router-dom';
 
 import useAuthProtection from '../../hooks/useAuthProtection';
 
+import type { Order } from '../../types';
+
 import { StyledOrders } from './orders.styled';
+
+const OrderItem = ({ data }: { data: Order }): JSX.Element => {
+    console.log(data);
+    return (
+        <li>
+            <span>{new Intl.DateTimeFormat('en-us', { dateStyle: 'medium' }).format(new Date(data.createdAt))}</span>
+        </li>
+    );
+};
 
 const Orders = (): JSX.Element => {
     useAuthProtection();
 
-    const orders = useLoaderData();
+    const orders = useLoaderData() as [Order];
+
     console.log(orders);
 
-    return <StyledOrders className="Orders">Orders</StyledOrders>;
+    return (
+        <StyledOrders className="Orders">
+            <h1>Your Orders</h1>
+            {orders.length > 0 && (
+                <ul>
+                    {orders.map(order => (
+                        <OrderItem key={order.id} data={order} />
+                    ))}
+                </ul>
+            )}
+        </StyledOrders>
+    );
 };
 
 export default Orders;
