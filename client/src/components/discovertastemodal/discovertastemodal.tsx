f7cd952b8a2c9cd2638d1ed846119b32468c7df0
Alex <hi@alexmenger.me>
2023-03-07 09:43:51 +0100

    Remove warnings

--- client/src/components/discovertastemodal/discovertastemodal.tsx
+++ client/src/components/discovertastemodal/discovertastemodal.tsx
@@ -5,56 +5,56 @@ import Button from '../button/button';
 import StyledDiscoverTasteModal from './discovertastemodal.styled';
 import leafletLeaves from '../../assets/leaflet-leaves.svg';
 
-const quizData = {
-    1: {
-        question: 'Do you prefer food and drinks plain or flavored?',
-        whyweask:
-            "There's Coke, and then there's Cherry Coke. There's frozen yogurt, and then there are fifteen toppings. Tea is the same – it can be consumed barebones or with additional flavors.",
-        answers: {
-            type: 'one',
-            data: [
-                { label: 'Plain', value: 'plain' },
-                { label: 'Flavored', value: 'flavored' }
-            ]
-        }
-    },
-    2: {
-        question: 'What types of flavor do you like most?',
-        whyweask:
-            'Tea comes in many forms, and even the one made from tea leaves has very distinct flavors depending on the way it was produced.',
-        answers: {
-            type: 'many',
-            data: [
-                { label: 'Citrus', value: 'citrus' },
-                { label: 'Floral', value: 'floral' },
-                { label: 'Herbal', value: 'herbal' },
-                { label: 'Earthy', value: 'earthy' },
-                { label: 'Vegetal', value: 'vegetal' }
-            ]
-        }
-    },
-    3: {
-        question: 'Do you like the buzz of caffeine?',
-        whyweask: "Most teas have it, some don't. Some people enjoy it, while some don't.",
-        answers: {
-            type: 'one',
-            data: [
-                { label: 'Yes', value: 'caffeine' },
-                { label: 'No', value: 'no-caffeine' }
-            ]
-        }
-    },
-    4: {
-        question: 'What types of tea do you prefer?',
-        whyweask: 'You might already have a certain taste for',
-        answers: { type: '', data: [{ label: '', value: '' }] }
-    },
-    5: {
-        question: '',
-        whyweask: '',
-        answers: { type: '', data: [{ label: '', value: '' }] }
-    }
-};
+// const quizData = {
+//     1: {
+//         question: 'Do you prefer food and drinks plain or flavored?',
+//         whyweask:
+//             "There's Coke, and then there's Cherry Coke. There's frozen yogurt, and then there are fifteen toppings. Tea is the same – it can be consumed barebones or with additional flavors.",
+//         answers: {
+//             type: 'one',
+//             data: [
+//                 { label: 'Plain', value: 'plain' },
+//                 { label: 'Flavored', value: 'flavored' }
+//             ]
+//         }
+//     },
+//     2: {
+//         question: 'What types of flavor do you like most?',
+//         whyweask:
+//             'Tea comes in many forms, and even the one made from tea leaves has very distinct flavors depending on the way it was produced.',
+//         answers: {
+//             type: 'many',
+//             data: [
+//                 { label: 'Citrus', value: 'citrus' },
+//                 { label: 'Floral', value: 'floral' },
+//                 { label: 'Herbal', value: 'herbal' },
+//                 { label: 'Earthy', value: 'earthy' },
+//                 { label: 'Vegetal', value: 'vegetal' }
+//             ]
+//         }
+//     },
+//     3: {
+//         question: 'Do you like the buzz of caffeine?',
+//         whyweask: "Most teas have it, some don't. Some people enjoy it, while some don't.",
+//         answers: {
+//             type: 'one',
+//             data: [
+//                 { label: 'Yes', value: 'caffeine' },
+//                 { label: 'No', value: 'no-caffeine' }
+//             ]
+//         }
+//     },
+//     4: {
+//         question: 'What types of tea do you prefer?',
+//         whyweask: 'You might already have a certain taste for',
+//         answers: { type: '', data: [{ label: '', value: '' }] }
+//     },
+//     5: {
+//         question: '',
+//         whyweask: '',
+//         answers: { type: '', data: [{ label: '', value: '' }] }
+//     }
+// };
 
 const Start = () => (
     <div className="DiscoverTasteModal__Start">
@@ -71,7 +71,8 @@ const Start = () => (
 );
 
 const DiscoverTasteModal = () => {
-    const [progress, setProgress] = useState(0);
+    // const [progress, setProgress] = useState(0);
+    const [progress] = useState(0);
 
     return (
         <StyledDiscoverTasteModal className="DiscoverTasteModal" title="Discover Your Taste" footer={null}>
